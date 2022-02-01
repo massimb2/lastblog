2828385fdfd5cf6e8d8a5594d291e23cc40a82fb
massimb2 <98814073+massimb2@users.noreply.github.com>
2022-02-01 15:32:36 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+    <a href="https://sevenimgs.com/%d8%ae%d9%84%d9%81%d9%8a%d8%a7%d8%aa-%d8%a7%d9%8a%d9%81%d9%88%d9%86-%d9%81%d8%ae%d9%85%d9%87/" target="_blank">خلفيات ايفون فخمة</a>
       </main>
 
       <Footer />
